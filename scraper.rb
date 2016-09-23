82942a676c923f2a4c0af05f20f1c47d270c4db4
Tony Bowden <tony@tmtm.com>
2016-09-23 14:59:09 +0100

    Keep existing data

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
+existing = EveryPolitician::Index.new.country("Isle-of-Man").legislature('House-of-Keys').popolo.persons.map(&:wikidata).compact
+
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/isle-of-man-house-of-keys-wikipedia', column: 'wikipedia__en')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: names })
